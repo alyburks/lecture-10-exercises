20c02f22ac68cd36088853e9013a555b58b3688e
alyburks <lexyburks@gmail.com>
2016-02-05 13:47:18 -0800

    Update exercise1.R

--- exercise1.R
+++ exercise1.R
@@ -3,19 +3,21 @@ library(jsonlite)
 
 # Create a variable `api_key` that stores your api key
 # Register your own API key here: https://developer.echonest.com/account/register
-
+api_key <- HTE6XFNPTRTQUIN2O
 
 # Create a variable that stores the name of your favorite artist
-
+fav_artist <- "J Cole"
 
 # Substitute the spaces in your band-name with '+'s using the `gsub` function
-
+fav_artist <- gsub(" ", "+" ,fav_artist)
 
 # Construct a seach query to hit the api.
 # See documentation/examples: http://developer.echonest.com/docs/v4/artist.html#biographies
+data_query <- paste0("http://developer.echonest.com/docs/v4/artist.html#biographies", "api_key=", api_key, "&name=", fav_artist)
 
 
 # Use fromJSON to retrieve the results
-
+data_query_df <- fromJSON(data_query)
 
 # Get the text of the (first) biography available
+biobraphy <- data_queary_df$biography
